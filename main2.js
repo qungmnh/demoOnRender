faa98e423ae0ab48a540db95b058df111171047f
qungmnh <106642260+qungmnh@users.noreply.github.com>
2023-02-07 09:04:28 +0700

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function(req,res){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><p>This is home Page.</p></body></html>');
+    res.end();
+})
+
+app.get('/student', function(req,res){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><p>This is student Page.</p></body></html>');
+    res.end();
+})
+
+app.get('/admin', function(req,res){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><p>This is student Page.</p></body></html>');
+    res.end();
+})
+
+const PORT = process.eventNames.PORT || 5000
+app.listen(PORT)
+console.log('Server is up and running')
