8b38593550eb92978e1f1e9d81ad49ad4fe665f2
qungmnh <106642260+qungmnh@users.noreply.github.com>
2023-02-07 11:02:25 +0700

    button AGAIN

--- main2.js
+++ main2.js
@@ -4,11 +4,8 @@ var app = express()
 app.get('/', function(req,res){
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<html><body><p>This is home Page.</p></body></html>');
+    res.write('<html><body><input type="button" value="Go back!" onclick="history.back()"><body></html>')
     res.end();
-    <button id="go-back">Go back!</button>
-        document.getElementById('go-back').addEventListener('click', () => {
-        history.back();
-    });
 })
 
 app.get('/student', function(req,res){
